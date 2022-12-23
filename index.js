2116f051b62d5f51770d06dcdc00c0d4cddaee42
RajKKapadia <raajforyou@gmail.com>
2022-12-23 22:04:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const axios = require('axios');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+const textGeneration = async (prompt) => {
+
+    try {
+        const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: `Human: ${prompt}\nAI: `,
+            temperature: 0.9,
+            max_tokens: 500,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0.6,
+            stop: ['Human:', 'AI:']
+        });
+    
+        return {
+            status: 1,
+            response: `${response.data.choices[0].text}`
+        };
+    } catch (error) {
+        return {
+            status: 0,
+            response: ''
+        };
+    }
+};
+
+
+const webApp = express();
+
+const PORT = process.env.PORT;
+
+webApp.use(express.urlencoded({ extended: true }));
+webApp.use(express.json());
+webApp.use((req, res, next) => {
+    console.log(`Path ${req.path} with Method ${req.method}`);
+    next();
+});
+
+
+webApp.get('/', (req, res) => {
+    res.sendStatus(200);
+});
+
+
+webApp.post('/dialogflow', async (req, res) => {
+    
+    let action = req.body.queryResult.action;
+    let queryText = req.body.queryResult.queryText;
+
+    if (action === 'input-unknown') {
+        let result = await textGeneration(queryText);
+        if (result.status == 1) {
+            res.send(
+                {
+                    fulfillmentText: result.response
+                }
+            );
+        } else {
+            res.send(
+                {
+                    fulfillmentText: `Sorry, I'm not able to help with that.`
+                }
+            );
+        }
+    } else {
+        res.sendStatus(200);
+    }
+});
+
+
+webApp.listen(PORT, () => {
+    console.log(`Server is up and running at ${PORT}`);
+});
